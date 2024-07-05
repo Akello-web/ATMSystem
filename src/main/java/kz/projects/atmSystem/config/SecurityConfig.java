1a0ccac34382ffeaa6c446866894e0f42e7b0d62
Akello-web <aklbktglv@gmail.com>
2024-07-05 20:40:11 +0500

    Configured Spring-Security

--- src/main/java/kz/projects/atmSystem/config/SecurityConfig.java
+++ src/main/java/kz/projects/atmSystem/config/SecurityConfig.java
@@ -1,33 +1,58 @@
 package kz.projects.atmSystem.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
 
-//  @Autowired
-//  private UserService userService;
-//
-//  @Override
-//  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//    auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
-//  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/auth/**").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin().loginProcessingUrl("/auth/login").permitAll()
-            .and()
-            .logout().logoutUrl("/auth/logout").permitAll();
+  @Bean
+  public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(User.withUsername("user")
+            .password(bCryptPasswordEncoder.encode("userPass"))
+            .roles("USER")
+            .build());
+    manager.createUser(User.withUsername("admin")
+            .password(bCryptPasswordEncoder.encode("adminPass"))
+            .roles("USER", "ADMIN")
+            .build());
+    return manager;
   }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                    authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                            .requestMatchers("/").permitAll()
+                            .requestMatchers("/admin/**").hasRole("ADMIN")
+                            .anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults())
+            .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    return http.build();
+  }
+
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 }
