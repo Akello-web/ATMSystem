9c31dddd27e6eaf93901a998e51840ddb630cc18
Akello-web <aklbktglv@gmail.com>
2024-07-19 17:11:47 +0500

    Changed TransactionDTO to a record

--- src/main/java/kz/projects/atmSystem/dto/TransactionDTO.java
+++ src/main/java/kz/projects/atmSystem/dto/TransactionDTO.java
@@ -1,21 +1,12 @@
 package kz.projects.atmSystem.dto;
 
 import kz.projects.atmSystem.model.TransactionType;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-public class TransactionDTO {
-  private Long id;
-
-  private TransactionType type;
-
-  private double amount;
-
-  private LocalDateTime date;
-
-  private UserDTO user;
-}
+public record TransactionDTO(
+        Long id,
+        TransactionType type,
+        double amount,
+        LocalDateTime date,
+        UserDTO user
+) {}
--- src/main/java/kz/projects/atmSystem/mapper/TransactionMapper.java
+++ src/main/java/kz/projects/atmSystem/mapper/TransactionMapper.java
@@ -7,18 +7,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class TransactionMapper {
   public static TransactionDTO toDto(Transaction transaction) {
-    if (transaction == null){
+    if (transaction == null) {
       return null;
     }
 
-    TransactionDTO dto = new TransactionDTO();
-    dto.setId(transaction.getId());
-    dto.setType(transaction.getType());
-    dto.setAmount(transaction.getAmount());
-    dto.setDate(transaction.getDate());
-    dto.setUser(UserMapper.toDto(transaction.getUser()));
-
-
-    return dto;
+    return new TransactionDTO(
+            transaction.getId(),
+            transaction.getType(),
+            transaction.getAmount(),
+            transaction.getDate(),
+            UserMapper.toDto(transaction.getUser())
+    );
   }
 }
