289ebbb77fe8551bd842211bc8052ea48ed699af
Akello-web <aklbktglv@gmail.com>
2024-07-19 17:05:52 +0500

    Changed UserDTO to a record

--- src/main/java/kz/projects/atmSystem/dto/UserDTO.java
+++ src/main/java/kz/projects/atmSystem/dto/UserDTO.java
@@ -1,19 +1,14 @@
 package kz.projects.atmSystem.dto;
 
 import kz.projects.atmSystem.model.Permissions;
-import lombok.Getter;
-import lombok.Setter;
+
 
 import java.util.List;
 
-@Getter
-@Setter
-public class UserDTO {
-  private Long id;
 
-  private String accountNumber;
-
-  private double balance;
-
-  private List<Permissions> permissionList;
-}
+public record UserDTO(
+        Long id,
+        String accountNumber,
+        double balance,
+        List<Permissions> permissionList
+) {}
--- src/main/java/kz/projects/atmSystem/mapper/UserMapper.java
+++ src/main/java/kz/projects/atmSystem/mapper/UserMapper.java
@@ -7,16 +7,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserMapper {
   public static UserDTO toDto(User user) {
-    if (user == null){
+    if (user == null) {
       return null;
     }
 
-    UserDTO userDTO = new UserDTO();
-    userDTO.setId(user.getId());
-    userDTO.setAccountNumber(user.getAccountNumber());
-    userDTO.setBalance(user.getBalance());
-    userDTO.setPermissionList(user.getPermissionList());
-
-    return userDTO;
+    return new UserDTO(
+            user.getId(),
+            user.getAccountNumber(),
+            user.getBalance(),
+            user.getPermissionList()
+    );
   }
 }
